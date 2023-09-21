d7f44a0ce7ab0f31452d7f792156c8fea3cd795c
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-21 20:04:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import { Statistics } from './Statistics/Statistics';
 export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);  
+  const [bad, setBad] = useState(0);
 
   function countTotalFeedback() {
     const total = good + neutral + bad;
@@ -19,22 +19,22 @@ export function App() {
     return positivePercentage;
   }
 
-  function handleClick (e) {
+  function handleClick(e) {
     const btnText = e.target.textContent;
     switch (btnText) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevGood => prevGood + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevNeutral => prevNeutral + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevBad => prevBad + 1);
         break;
       default:
         return;
     }
-  };
+  }
 
   return (
     <Container>
